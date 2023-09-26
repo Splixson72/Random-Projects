c92c82e74293c90f2865357323dffaf5c41ab29a
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-09-26 14:24:37 +0100

    Add files via upload
    Project Euler #4 Largest Palindrome Product

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
 ﻿//Tuesday 26th September 2023
-//Project Euler 3
+//Project Euler 4
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PEProject3
+namespace PEProject4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Int64 numberToFactorise = 600851475143;
-            //Int64 numberToFactorise = 13195;
-            for (Int64 i = 2; i < Math.Sqrt(numberToFactorise); i++) //Math.Sqrt(i) is the highest a factor of i can be except itself
+            int PalidromeProduct;
+            int highScore=0;
+            string stringPalindrome;
+            for (int i = 100; i < 1000; i++)
             {
-                if (numberToFactorise % i == 0) //this checks if it is a factor
+                for (int j = 100; j < 1000; j++)
                 {
-                    numberToFactorise = numberToFactorise / i; //by continually making the number smaller, we eliminate the factors so to not repeat them
-                    Console.WriteLine(i);
-                    Console.WriteLine(numberToFactorise);
-                    i = 2; //we start at 2 to avoid continually dividing by 1 (which is not prime)
+                    PalidromeProduct = i * j;
+                    stringPalindrome = PalidromeProduct.ToString();
+                    if (highScore < PalidromeProduct)
+                    {
+                        if (stringPalindrome[stringPalindrome.Length - 1] == stringPalindrome[0])
+                        {
+                            if (stringPalindrome[stringPalindrome.Length - 2] == stringPalindrome[1])
+                            {
+                                if (stringPalindrome[stringPalindrome.Length - 3] == stringPalindrome[2])
+                                {
+                                    highScore = PalidromeProduct;
+                                    Console.WriteLine(stringPalindrome);
+                                    Console.WriteLine(i + " and " + j);
+                                }
+                            }
+                        }
+                    }
                 }
             }
             Console.ReadLine();
