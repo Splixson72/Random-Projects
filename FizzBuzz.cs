b264453f93effa369d071b75238273975e41c3ee
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-09-26 11:57:01 +0100

    Add files via upload
    FizzBuzz Program using C# (1 to 100)

---
+++ FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿//Tuesday 26th September 2023
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+            for (int i = 1; i < 101; i++) //i++ is i += 1
+            {
+                if (i % 5 == 0 & i % 3 == 0) //& is AND
+                {
+                    Console.WriteLine("FizzBuzz");       
+                    }
+                else if (i % 5 == 0) // == is how you do equals
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0) //% is Modulo
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                    Console.WriteLine(i);    
+                // No EndIf, the semicolon symbolises it
+            }
+            Console.ReadLine();
+        }
+    }
+}
