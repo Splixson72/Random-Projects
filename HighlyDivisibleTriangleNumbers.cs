d8f5b07565fa4ee36d55b85463979abb37c54a02
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-10-16 15:05:24 +0100

    Update HighlyDivisibleTriangleNumbers.cs
    Too Slow but working on it

--- HighlyDivisibleTriangleNumbers.cs
+++ HighlyDivisibleTriangleNumbers.cs
@@ -1,12 +1,10 @@
-﻿//Thursday 16th October 2023
+//Thursday 16th October 2023
 //Highly Divisible Triangle Numbers
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace HighlyDivisibleTriangleNumbers
 {
     internal class Program
@@ -15,23 +13,38 @@ namespace HighlyDivisibleTriangleNumbers
         {
             int currentTri = 0;
             int triAdd = 1;
-            int numFactors = 0;
+            int numFactors = 1;//1 is always a factor
+            int highFactor = 0;
             for (int i = 0; i < 2000000; i++)
             {
                 currentTri += triAdd;
                 triAdd++;
-                for (int j = 2; j < currentTri; j++) //going through a billion numbers
+                for (int j = 2; j <= currentTri; j++) //going through a billion numbers
                 {
-                  
-                        if (currentTri % j == 0) //this checks if it is a factor
+                    if (currentTri % j == 0) //this checks if it is a factor
+                    {                   
+                        numFactors += 1;
+                        if (j == currentTri)
                         {
-                            currentTri = currentTri / j; //by continually making the number smaller, we eliminate the factors so to not repeat them                                                               
-                            j = 2; //we start at 2 to avoid continually dividing by 1 (which is not prime)
+                            Console.WriteLine(numFactors + " " + currentTri);
+                            if (numFactors > highFactor)
+                            {
+                                Console.WriteLine(numFactors + " " + currentTri);
+                                highFactor = numFactors;
+                                numFactors = 1;
+                                j = 2;
+                            }
                         }
-                    }                  
-                    
+                    }
+                  
+                }
+                numFactors = 0;
+                if (highFactor >= 500)
+                {
+                    Console.WriteLine(i);
                 }
             }
+            Console.ReadLine();
         }
     }
 }
