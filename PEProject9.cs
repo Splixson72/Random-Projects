6b852a3dd1bb1c86bdfef98f0fa7113aab0cb327
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-10-24 11:05:58 +0100

    Add files via upload

---
+++ PEProject9.cs
@@ -0,0 +1,41 @@
+﻿//Tuesday 24th October 2023
+//Special Pythagoran Triplet
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProject9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a<b<c
+            //a^2 + b^2 = c^2
+            //a + b + c = 1000
+            //find ABC
+            Double a = 0; //m^2 - n^2
+            Double b = 0; //2mn
+            Double c = 0; //m^2 + n^2
+            //C has to be less than root 998 obv so 31
+            for (int m = 0; m < 23; m++)
+            {
+                for (int n = 0; n < 23; n++)
+                {
+                    a = Math.Pow(m, 2) - Math.Pow(n, 2);
+                   b = 2 * m * n;
+                    c = Math.Pow(m, 2) + Math.Pow(n, 2);
+                    if (a + b + c == 1000)
+                    {
+                        Console.WriteLine(a + " " + b + " " + c);
+                        Console.WriteLine(a * b * c);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
