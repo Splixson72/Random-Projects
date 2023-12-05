7ef8d0f9ee2e09f596da4cec40a3a0f5f008f303
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-12-05 17:22:18 +0000

    Add files via upload

---
+++ RitangleQ24.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BasicallyDual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1;
+            string num2;
+            int total =0;
+            for (int i = 10; i < 100; i++)
+            {
+                for (int j = 10; j < 100; j++)
+                {
+                    num1 = Convert.ToString(i);
+                    num2 = Convert.ToString(j);   
+                    if ((Convert.ToInt32(num1[0]) - '0') * j + (Convert.ToInt32(num1[1]) - '0') == (Convert.ToInt32(num2[0]) - '0') *i + (Convert.ToInt32(num2[1] - '0')) && i!=j)
+                    {
+                       // Console.WriteLine(i + " " + ((Convert.ToInt32(num1[0]) - '0') * j + (Convert.ToInt32(num1[1]) - '0'))); 
+                       // Console.WriteLine(j + " " + ((Convert.ToInt32(num2[0]) - '0') * i + (Convert.ToInt32(num2[1] - '0'))));
+                        if (i+j>total)
+                        {
+                            total = i + j;
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine(i + " " + j + " " + (i + j));
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }                       
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
