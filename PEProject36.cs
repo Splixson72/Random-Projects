2c6e3d8b5d905d48409faa2c578760fd8fb28ea7
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-12-13 10:15:20 +0000

    Add files via upload

---
+++ PEProject36.cs
@@ -0,0 +1,38 @@
+﻿//Wednesday 13th Decenber 2023
+//Problem 36 - Double-base Palidromes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProject36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            string yarn = ""; 
+            for (int i = 0; i < 1000000; i++)
+            {
+                yarn = Convert.ToString(i);
+                Console.WriteLine(i + " " + yarn + " " +yarn.Reverse());
+                if (Convert.ToString(i) == Convert.ToString(Convert.ToString(i).Reverse()))
+                {
+                    Console.WriteLine(i);
+                    if (true)
+                    {
+                        if (Convert.ToString(i,2) == Convert.ToString(Convert.ToString(i, 2).Reverse()))
+                        {
+                            Console.WriteLine(i + " " + Convert.ToString(i, 2));
+                            total += i;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
