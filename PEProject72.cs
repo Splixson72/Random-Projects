7d172aa20254dd41f88b7ce779ff2eb357f42fb4
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2023-12-20 09:16:08 +0000

    Update PEProject72.cs

--- PEProject72.cs
+++ PEProject72.cs
@@ -14,24 +14,18 @@ namespace PEProject72
         static void Main(string[] args)
         {
             Program p = new Program();
-            //297636497331 Generated answer for D = 1000000
-            int maxDenominator = 8;
+            //303963552391 Real Generated Answer with no repeated factors computed
+            int maxDenominator = 1000000;
             double total = 0;
             for (long i = 2; i <= maxDenominator; i++) //dividing by one is not a fraction
             {
                 double coprime = 1;
                 List<long> fractions = p.PrimeDivisors(i);
-                //    Console.WriteLine(i + " " + fractions.Count);
-                Console.WriteLine("Factors of " + i);
-                for (int m = 0; m < fractions.Count; m++) //OUTPUT ALL FACTORS FOR CHECKING
-                {
-                    Console.WriteLine(fractions[m]);
-                }
                 for (int h = 0; h < fractions.Count; h++)
                 {
                     double quotient = (double)1 / fractions[h];
                     coprime *= (1 - quotient);
-           
+
                 }
                 coprime *= i;
                 total += coprime;
@@ -43,38 +37,49 @@ namespace PEProject72
         {
             List<long> Prime = new List<long>();
             long j = numberToFactorise;
+          
             for (Int64 i = 2; i <= Math.Sqrt(j); i++) //Math.Sqrt(i) is the highest a factor of i can be except itself
             {
+                bool contains = false;
                 if (numberToFactorise % i == 0) //this checks if it is a factor
                 {
                     numberToFactorise /= i; //by continually making the number smaller, we eliminate the factors so to not repeat them
                     if (!Prime.Contains(i))
                     {
-                        Prime.Add(i);
+                        for (int p = 0; p < Prime.Count; p++) //because it will see something like 4, we need to see if its not already counted
+                        {
+                            if (i % Prime[p] == 0)
+                            {
+                                contains = true;
+                            }
+                        }
+                        if (contains == false)
+                        {
+                            Prime.Add(i);
+                        }
+                        
                     }
                     i = 2; //we start at 2 to avoid continually dividing by 1 (which is not prime)
                 }
-            }
-            //long k = numberToFactorise;
-            //do
-            //{         
-            //    for (int i = 0; i < Prime.Count; i++)
-            //    {
-            //        if (numberToFactorise % Prime[i] == 0)
-            //        {
-            //            numberToFactorise /= Prime[i];
-            //            Console.WriteLine("divided by " + Prime[i]);
-            //           // final = false;
-            //        }
-            //    }
-            //} while ((k==numberToFactorise) || (numberToFactorise!=1));        
-
-            if (numberToFactorise==j)
+            }      
+             int counter = 0;
+            do
+            {
+                 counter = 0;
+                for (int i = 0; i < Prime.Count; i++)
+                {
+                    if (numberToFactorise % Prime[i] ==0) //keep dividing the quotient by primes to get the final primes
+                    {
+                        numberToFactorise /= Prime[i];
+                        counter += 1; ;
+                    }
+                }
+            } while (counter != 0);
+            if (numberToFactorise!=1) //add the final prime, which will always be above the square root
             {
                 Prime.Add(numberToFactorise);
-            }      
-           
+            }        
             return Prime;
-        }   
+        }
     }
 }
